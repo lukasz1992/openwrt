89dbcd7454d468e8fe2959852e445e92b20aed36
Lukasz M <lukasz1992@github.com>
2024-08-25 14:28:31 +0200

    Update Mediatek firmware

--- package/firmware/linux-firmware/mediatek.mk
+++ package/firmware/linux-firmware/mediatek.mk
@@ -73,7 +73,7 @@ Package/mt7981-wo-firmware = $(call Package/firmware-default,MT7981 offload firm
 define Package/mt7981-wo-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware/mediatek
 	$(INSTALL_DATA) \
-		$(PKG_BUILD_DIR)/mediatek/mt7981_wo.bin \
+		./files/mediatek/mt7981_wo.bin \
 		$(1)/lib/firmware/mediatek
 endef
 $(eval $(call BuildPackage,mt7981-wo-firmware))
@@ -82,8 +82,8 @@ Package/mt7986-wo-firmware = $(call Package/firmware-default,MT7986 offload firm
 define Package/mt7986-wo-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware/mediatek
 	$(INSTALL_DATA) \
-		$(PKG_BUILD_DIR)/mediatek/mt7986_wo_0.bin \
-		$(PKG_BUILD_DIR)/mediatek/mt7986_wo_1.bin \
+		./files/mediatek/mt7986_wo_0.bin \
+		./files/mediatek/mt7986_wo_1.bin \
 		$(1)/lib/firmware/mediatek
 endef
 $(eval $(call BuildPackage,mt7986-wo-firmware))
